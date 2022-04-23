5a2f6771b991c90a535d28fb21f8c3e341d1de25
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-23 18:46:41 -0500

    Agregar una nueva ruta que reciba Query Params

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.get('/explorersInNode', (req, res) => {
     res.send(explorer);
 });
 
+// Query Params: Recibir parametros por la URL
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params);
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
