5c07a81e5cc0cc424cbad7977d0c0019498efb66
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-23 17:58:53 -0500

    creation of first server with express

---
+++ app.js
@@ -0,0 +1,18 @@
+// Usando objeto express, importando modulo
+const express = require('express');
+
+// App de Express
+const app = express();
+
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000;
+
+// Path inicial, respondera a la url localhost:3000
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
